e1ff51060cefcb38766a472ee3f143c78fa69811
Kiri Um <kirirotha@gmail.com>
2020-07-16 15:32:37 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,16 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
+  @@GENRES = []
   def initialize(title, genre)
     @title = title
-    @GENRES
+    @GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
-  de
+  def 
 
 end
