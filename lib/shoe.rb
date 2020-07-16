8dbb257b45df7d205c94e0e15f1bf438e95e577e
Kiri Um <kirirotha@gmail.com>
2020-07-16 16:03:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    @brands << brand
+    BRANDS << brand
   end
 
   def cobble
@@ -14,9 +14,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << @brand
+  def brands
   end
 
 end
