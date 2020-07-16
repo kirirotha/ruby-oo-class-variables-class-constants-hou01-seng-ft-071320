e8e1346db4a9850c88502d73f1c72847812fe54c
Kiri Um <kirirotha@gmail.com>
2020-07-16 16:09:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,11 @@ class Shoe
   BRANDS = []
 
   def initialize(brand)
-    @brand = brand
-    BRANDS << brand
-    BRANDS.uniq
+    @brand = brand|
+    if BRANDS.!include?(brand)
+        BRANDS << brand
+
+    end
   end
 
   def cobble
